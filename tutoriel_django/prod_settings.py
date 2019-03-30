e643c7f17bec5158c1fe2238cc6c80e8a41d98e0
Maurel OUSSOU <micronet@consultant.com>
2019-03-30 18:22:55 +0100

    Config DATABASES URL

--- tutoriel_django/prod_settings.py
+++ tutoriel_django/prod_settings.py
@@ -1,8 +1,12 @@
+import dj_database_url
+
 from tutoriel_django.settings import *
 
 DEBUG = False
 
 TEMPLATE_DEBUG = False
+
+DATABASES['default'] = dj_database_url.config()
 
 SECRET_KEY = 'ocx4+waga8*@3&=r^=c0php+8^#y4y6%ik6xv_l&dzii*mr^n_'
 
