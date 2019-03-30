f415d69f719c3c116c5bf022092eba24a1b17c71
Maurel OUSSOU <micronet@consultant.com>
2019-03-30 17:30:23 +0100

    Add heroku subdomain to ALLOWED_HOSTS

--- tutoriel_django/settings.py
+++ tutoriel_django/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '+m4kfmw8%t$9b005w!bmw=g+@b0g626sfb3f!!y%%a-76r-*w7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['easy-blog-django.herokuapp.com']
 
 
 # Application definition
